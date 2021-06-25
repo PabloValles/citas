8ec04380cb941e757518d373e63d59ef39329d62
PabloValles <valles.pablo@hotmail.com>
2021-06-25 16:43:54 -0300

    1er commit - Terminando componente Formulario

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Formulario from './components/Formulario'
 
 function App() {
+
+  // Arreglo de citas (todas)
+  const [citas, guardarCitas] = useState([])
+
+
+  // Funcion que tome las citas actuales y agregue la nueva
+  const crearCita = cita => {
+    guardarCitas([...citas, cita])
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Administrador de pacientes</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario 
+              crearCita={crearCita}
+            />
+          </div>
+          <div className="one-half column">
+
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+const Formulario = ({crearCita}) => {
+
+    //Crear state de citas
+    const [cita, actualizarCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+    });
+
+    const [error, actualizarError] = useState(false)
+
+    //Funcion q se ejecuta cdo el usuario escriba en un imput
+    const actualizarState = e => {
+        //console.log(e.target.value)
+        actualizarCita({
+            ...cita,
+            [e.target.name] : e.target.value
+        })
+
+    }
+
+    // Extraer los valores de la cita
+    const {mascota, propietario, fecha, hora, sintomas} = cita;
+
+    // Cuando el usuario envía el formulario
+    const submitCita = e => {
+        e.preventDefault();
+
+        // Validar
+        if(mascota.trim() === "" || propietario.trim() === "" || fecha.trim() === "" || hora.trim() === "" || sintomas.trim() === "" ){
+            //console.log("Hay un error")
+            actualizarError(true)
+            return;
+        }
+
+        // Eliminar el mensaje previo
+        actualizarError(false)
+
+        // Asignar un ID
+        cita.id = uuidv4()
+
+        // Crear la cita
+        crearCita(cita)
+
+        // Reiniciar el Form cn el modificador del state
+        actualizarCita({
+            mascota: '',
+            propietario: '',
+            fecha: '',
+            hora: '',
+            sintomas: ''
+        })
+    }
+
+
+    return ( 
+        <> 
+            <h2>Crear citas</h2>
+
+            { error ? <p className="alerta-error"> Todos los campos son obligatorios</p> : null }
+
+            <form 
+                onSubmit={submitCita}
+            >
+                <label>Nombre mascota</label>
+                <input 
+                    type="text"
+                    name="mascota"
+                    className="u-full-width"
+                    placeholder="Nombre de la mascota"
+                    onChange={actualizarState}
+                    value={mascota}
+                />
+                <label>Nombre Dueño</label>
+                <input 
+                    type="text"
+                    name="propietario"
+                    className="u-full-width"
+                    placeholder="Nombre del dueño de la mascota"
+                    onChange={actualizarState}
+                    value={propietario}
+                />
+                <label>Fecha</label>
+                <input 
+                    type="date"
+                    name="fecha"
+                    className="u-full-width"
+                    onChange={actualizarState}
+                    value={fecha}
+                />
+                <label>Hora</label>
+                <input 
+                    type="time"
+                    name="hora"
+                    className="u-full-width"
+                    onChange={actualizarState}
+                    value={hora}
+                />
+                <label>Síntomas</label>
+                <textarea
+                    className="u-full-width"
+                    name="sintomas"
+                    onChange={actualizarState}
+                    value={sintomas}
+                >
+                </textarea>
+                <button 
+                    type="submit"
+                    className="u-full-width button-primary"
+                >Agregar cita</button>
+            </form>
+        </>
+
+     );
+}
+ 
+export default Formulario;
